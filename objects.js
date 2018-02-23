354fe69da01591fcc0da0aa2b18fcadca588de8c
Eli Draluk <eli.draluk@gmail.com>
2018-02-23 17:45:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {'Pink Floyd':'Dark Side of The Moon'}
 
-var updatePlaylist(playlist,artist,song){
+function updatePlaylist(playlist,artist,song){
   return Object.assign({},playlist,{[artist]: song})
 }
